6603142f8a81bd05d05b0d2b9361422632ac9c90
Tammy Lam <lamtammy1515@gmail.com>
2020-06-12 21:59:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,12 +2,12 @@
 
 def introduction(name)
   puts "Hi, my name is #{name}."
-end 
+end
 
-introduction("Josh")
+introduction("josh")
 
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end 
+  puts "hi, my name is #{name} and i am learning to program in #{language}."
+end
 
-introduction_with_language("Dan", "Ember.js")
+introduction_with_language("dan", "ember.js")
